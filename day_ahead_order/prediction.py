c23b117200685adb434e9df0f264bfd9f3a6b11f
Artūrs Gailītis <arturs.gailitis@gmail.com>
2024-11-02 16:10:54 +0100

    (fix) [PredictionService] indentation of helper methods and scope of 'connection'; (refactor) extract outline of regular consumer prediction to separate method (deserves class?)

--- day_ahead_order/prediction.py
+++ day_ahead_order/prediction.py
@@ -50,7 +50,7 @@ class PredictionService:
         # TODO: clarify policy or assume it, documenting my assumptions!
         if prediction_day < date.today():
             # TODO: allow to cross-check predictions with past consumption records?
-            #   -> yes: assert prediction_day falls within self.connection [active_from; active_until] date range
+            #   -> yes: assert prediction_day falls within connection [active_from; active_until] date range
             #   -> no: raise ValueError("Prediction day should be in future")
             # )
             pass
@@ -64,14 +64,14 @@ class PredictionService:
             #   -> no: raise ValueError("Prediction day should be in future")
             pass
 
-        if not self.connection.is_active_on(prediction_day):
+        if not connection.is_active_on(prediction_day):
             # TODO: would it make sense to offer consumption predictions outside
             #   of contract period for the consumer?
             #   It kind of makes sense when:
             #   - grid connection is expired, but might consider to reopen contract in future?
-            #   - self.connection.active_from is still in future (not active yet)
+            #   - connection.active_from is still in future (not active yet)
             #
-            #   -> no: raise ValueError("GridConnection {self.connection} is not active on {prediction_day}")
+            #   -> no: raise ValueError("GridConnection {connection} is not active on {prediction_day}")
             pass
 
         # Main algo - predict based on connection type (regular/solar/mixed).
@@ -83,51 +83,23 @@ class PredictionService:
         )  # Based upon: https://stackoverflow.com/a/69275860
 
         if connection.prediction_type == PredictionType.regular:
-            # As defined in epic:
-            #
-            # Companies usually follow a repetitive, weekly pattern
-            # -> use historic data of similar weekdays to make a prediction:
-            #   • (1) For predicting a Monday, average the values of the past 3 Mondays (same for Tuesday, Wednesday, etc.)
-            #   • (2) If not available, use as many Mondays as available)
-            #   • (3) If no Mondays are present at all, use average yearly value (from GridConnection)
-
-            # Thoughts:
-            # - if self.connection has active_from: ensure we do not request
-            #       historical weekday consumption before that
-            # - if self.connection has active_until:
-            #       - if connection expired before last weekday_of(prediction_day):
-            #           -> skip (1) and begin with (2), beginning from last weekday_of(prediction_day) before self.connection.active_until
-
-            # (i) HistoricConsumptionService returns time series with step of 15 minutes -
-            #     should we aggregate in pandas DataFrame, and apply a process like
-            #     groupBy [split-apply-combine](https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#group-by-split-apply-combine)
-            #     to returned historical consumoption data
-            #     to get expected time granuality of 60 minutes?
-
-            # (i) fallback to average yearly consumption -> calculate avg hourly consumption:
-            #   - Assume 365 days per year, as GridConnection has no info of
-            #     how many leap years had contributed to calculating annual consumption
-            #   - avg_hourly_consumption = self.connection.standard_yearly_consumption / (365 * 24)
-
-            # TODO: implement:
-            # predicted = self.predict_regular_connection_consumption(prediction_day)
-            pass
+            return self.predict_regular_connection_consumption(
+                prediction_day, connection
+            )
 
         elif connection.prediction_type == PredictionType.solar:
-            # TODO: calculate prediction_range in UCT
-
+            # TODO: extract to method
             predict_from, predict_to = self.get_prediction_range(prediction_day)
 
-            # Can be returned as result - SolarForecastService already indexes
-            # series as expected in API contract
-            predicted = self.solar_forecast(
+            # SolarForecastService already indexes series by hour (as expected in API contract)
+            return self.solar_forecast(
                 predict_from,
                 predict_to,
-                self.connection.latitude,
-                self.connection.longitude,
-                self.connection.solar_plane_declination,
-                self.connection.solar_plane_azimuth,
-                self.connection.solar_rated_power,
+                connection.latitude,
+                connection.longitude,
+                connection.solar_plane_declination,
+                connection.solar_plane_azimuth,
+                connection.solar_rated_power,
             )
 
         elif connection.prediction_type == PredictionType.mixed_solar_regular:
@@ -155,31 +127,64 @@ class PredictionService:
         #      is last hour of previous date in UTC.
         return predicted
 
-        def approximate_yearly_conumption_as_prediction(
-            self, connection: GridConnection, prediction_day: date
-        ) -> Series:
-            predict_from, predict_to = self.get_prediction_range(prediction_day)
-            hours = date_range(predict_from, predict_to, freq="60T")
+    def predict_regular_connection_consumption(
+        self, prediction_day: date, connection: GridConnection
+    ) -> Series:
 
-            avg_kwh_per_hour = self.avg_hourly_consumption(connection)
-            return Series(index=hours, data=avg_kwh_per_hour)
+        # As defined in epic:
+        #
+        # Companies usually follow a repetitive, weekly pattern
+        # -> use historic data of similar weekdays to make a prediction:
+        #   • (1) For predicting a Monday, average the values of the past 3 Mondays (same for Tuesday, Wednesday, etc.)
+        #   • (2) If not available, use as many Mondays as available)
+        #   • (3) If no Mondays are present at all, use average yearly value (from GridConnection)
 
-        def avg_hourly_consumption(self, connection: GridConnection) -> float:
-            """
-            When no suitable historical data available for regular/mixed
-            grid connections, approximate hourly consumption/production based
-            upon historically recorded annual consumption (in kWh).
+        # Thoughts:
+        # - if connection has active_from: ensure we do not request
+        #       historical weekday consumption before that
+        # - if connection has active_until:
+        #       - if connection expired before last weekday_of(prediction_day):
+        #           -> skip (1) and begin with (2), beginning from last weekday_of(prediction_day) before connection.active_until
 
-            Args:
-                connection
-            Returns:
-                float (kWh consumed or produced on average per hour)
-            """
-            # TODO: could refine a bit by taking into account how many leap
-            #   years happened to be during period of:
-            #   [connection.active_from; active_until|today]
-            hours_per_year = 8760  # 365 * 24
-            return connection.standard_yearly_consumption / hours_per_year
+        # (i) HistoricConsumptionService returns time series with step of 15 minutes -
+        #     should we aggregate in pandas DataFrame, and apply a process like
+        #     groupBy [split-apply-combine](https://pandas.pydata.org/pandas-docs/stable/user_guide/groupby.html#group-by-split-apply-combine)
+        #     to returned historical consumoption data
+        #     to get expected time granuality of 60 minutes?
+
+        # (i) fallback to average yearly consumption -> calculate avg hourly consumption:
+        #   - Assume 365 days per year, as GridConnection has no info of
+        #     how many leap years had contributed to calculating annual consumption
+        #   - avg_hourly_consumption = connection.standard_yearly_consumption / (365 * 24)
+
+        # TODO: implement:
+        pass
+
+    def approximate_yearly_conumption_as_prediction(
+        self, connection: GridConnection, prediction_day: date
+    ) -> Series:
+        predict_from, predict_to = self.get_prediction_range(prediction_day)
+        hours = date_range(predict_from, predict_to, freq="60T")
+
+        avg_kwh_per_hour = self.avg_hourly_consumption(connection)
+        return Series(index=hours, data=avg_kwh_per_hour)
+
+    def avg_hourly_consumption(self, connection: GridConnection) -> float:
+        """
+        When no suitable historical data available for regular/mixed
+        grid connections, approximate hourly consumption/production based
+        upon historically recorded annual consumption (in kWh).
+
+        Args:
+            connection
+        Returns:
+            float (kWh consumed or produced on average per hour)
+        """
+        # TODO: could refine a bit by taking into account how many leap
+        #   years happened to be during period of:
+        #   [connection.active_from; active_until|today]
+        hours_per_year = 8760  # 365 * 24
+        return connection.standard_yearly_consumption / hours_per_year
 
         # TODO: return Pandas date_range instead? (https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#generating-ranges-of-timestamps)
         # TODO: extract to something like datetime_utils.py ?
